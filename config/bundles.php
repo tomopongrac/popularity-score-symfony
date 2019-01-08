5c1a8087f6f2621fd7232f4a2ddb0b17270ee959
Tomislav Pongrac <tomo.pongrac@gmail.com>
2019-01-08 16:26:20 +0100

    Installing oauth client credentials.

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Trikoder\Bundle\OAuth2Bundle\TrikoderOAuth2Bundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
---
+++ src/Command/OauthInstallCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
+
+class OauthInstallCommand extends Command
+{
+    protected static $defaultName = 'app:oauth:install';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Installing')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $process = new Process('mkdir var/oauth');
+        $process->run();
+
+        $process = new Process('openssl genrsa -out var/oauth/private.key 2048');
+        $process->run();
+
+        $process = new Process('chmod 644 var/oauth/private.key');
+        $process->run();
+
+        $process = new Process('openssl rsa -in var/oauth/private.key -pubout -out var/oauth/public.key');
+        $process->run();
+
+        $process = new Process('chmod 644 var/oauth/public.key');
+        $process->run();
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+    }
+}
